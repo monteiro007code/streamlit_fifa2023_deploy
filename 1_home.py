c7a402bcaab828ad3faf965f63c7736532d56c6a
Rodrigo <rm8545735@gmail.com>
2026-02-18 20:12:52 -0300

    revisão código

--- 1_home.py
+++ 1_home.py
@@ -1,10 +1,17 @@
-import streamlit as st
-import pandas as pd
 import webbrowser
 from datetime import datetime
 
-if "data" not in st.session_state: # Verifica se os dados já estão carregados na sessão
-    df_data = pd.read_csv("datasets\CLEAN_FIFA23_official_data.csv", index_col=0)
+import pandas as pd
+import streamlit as st
+
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv(
+        "datasets/CLEAN_FIFA23_official_data.csv",
+        index_col=0,
+        skipinitialspace=True,
+    )
+    df_data.columns = df_data.columns.str.strip().str.replace("Â£", "£", regex=False)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
@@ -16,4 +23,6 @@ st.sidebar.markdown("Desenvovido por [Asimov Academy](https://www.asimov.academy
 
 btn = st.button("Acesse os dados no Kaggle")
 if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/stefanoleone992/fifa-23-complete-player-dataset")  
+    webbrowser.open_new_tab(
+        "https://www.kaggle.com/datasets/stefanoleone992/fifa-23-complete-player-dataset"
+    )
